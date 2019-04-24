035bb3227a3b336691891385700822cbdedd9866
kalitawojciech <wojtek1311997@gmail.com>
2019-04-24 22:25:44 +0200

    created ProductEntity Entity

---
+++ crud/crud.Core/Entities/ProductEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace crud.Core.Entities
+{
+    public class ProductEntity
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
