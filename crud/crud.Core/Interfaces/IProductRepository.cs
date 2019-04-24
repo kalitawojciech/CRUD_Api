7fd954a2758ff8cb917f1da1c737dcc619d4ebd1
kalitawojciech <wojtek1311997@gmail.com>
2019-04-24 22:30:51 +0200

    created interface IProductRepository

---
+++ crud/crud.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using crud.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crud.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        ProductEntity GetProduct(int id);
+        IEnumerable<ProductEntity> GetProducts();
+    }
+}
