be4662a9fdba66e9fc11c81bdc67ef50d3903fc4
kalitawojciech <wojtek1311997@gmail.com>
2019-04-25 15:33:24 +0200

    fixed problems

--- crud/crud.Api/Startup.cs
+++ crud/crud.Api/Startup.cs
@@ -6,17 +6,33 @@ using crud.Core.Interfaces;
 using crud.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace crud.Api
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var ConnectionString = Configuration["ConnectionString:ProductDBConnectionString"];
+            services.AddDbContext<ProductContext>(o => o.UseSqlServer(ConnectionString));
+
             services.AddTransient<IProductRepository, ProductRepository>();
         }
 
@@ -27,11 +43,14 @@ namespace crud.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
