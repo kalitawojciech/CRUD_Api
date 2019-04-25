a648fbf0526bc080864fa7afc8bc287d9f3e85ff
kalitawojciech <wojtek1311997@gmail.com>
2019-04-25 12:04:45 +0200

    created ProductContext class

--- crud/crud.Api/Startup.cs
+++ crud/crud.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using crud.Core.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,7 @@ namespace crud.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddTransient<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ crud/crud.Infrastructure/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using crud.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crud.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
