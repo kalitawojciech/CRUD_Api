10e4dba2a55b09ed310a707978b96cf687ef9d38
kalitawojciech <wojtek1311997@gmail.com>
2019-04-25 12:18:33 +0200

    created and implemented ProductRepository class

--- crud/crud.Api/Startup.cs
+++ crud/crud.Api/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using crud.Core.Interfaces;
+using crud.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,7 @@ namespace crud.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ crud/crud.Infrastructure/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using crud.Core.Entities;
+using crud.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace crud.Infrastructure
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ProductContext _context;
+
+        public ProductRepository(ProductContext context)
+        {
+            _context = context;
+        }
+        public ProductEntity GetProduct(int id)
+        {
+            return _context.Products.Where(p => p.ProductId == id).FirstOrDefault();
+        }
+
+        public IEnumerable<ProductEntity> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
