eb7512eeac0fe376c4c9d432537429f84305a9c1
kalitawojciech <wojtek1311997@gmail.com>
2019-04-25 16:10:32 +0200

    seeding data

--- crud/crud.Infrastructure/ProductContext.cs
+++ crud/crud.Infrastructure/ProductContext.cs
@@ -14,5 +14,26 @@ namespace crud.Infrastructure
         }
 
         public DbSet<ProductEntity> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductEntity>().HasData(
+                new ProductEntity()
+                {
+                    ProductId = 1,
+                    ProductName = "Ball",
+                    ProductDescription = "The best ball to play football",
+                    ProductPrice = 29.9M
+                },
+                new ProductEntity()
+                {
+                    ProductId = 2,
+                    ProductName = "T-shirt",
+                    ProductDescription = "Blue T-shirt",
+                    ProductPrice = 19.9M
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
