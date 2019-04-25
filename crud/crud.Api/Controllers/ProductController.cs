a2cdfe4eae3460dc82915f93243526c04df392ae
kalitawojciech <wojtek1311997@gmail.com>
2019-04-25 15:53:28 +0200

    created controller ProductController and inside this controller implemented GetProducts method

---
+++ crud/crud.Api/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using crud.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Api.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult GetProducts()
+        {
+            var productEntities = _productRepository.GetProducts();
+            return Ok(productEntities);
+        }
+    }
+}
