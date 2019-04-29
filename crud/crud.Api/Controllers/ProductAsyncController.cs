ec1f303d724c8e161489bf7841ba4eff168807d5
kalitawojciech <wojtek1311997@gmail.com>
2019-04-29 22:15:02 +0200

    created empty ProductAsyncController

---
+++ crud/crud.Api/Controllers/ProductAsyncController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Api.Controllers
+{
+    [Route("api/product/async")]
+    [ApiController]
+    public class ProductAsyncController : ControllerBase
+    {
+    }
+}
