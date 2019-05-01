38ed5eb56e705cc9536a699390801137be94cc2a
kalitawojciech <wojtek1311997@gmail.com>
2019-05-01 13:32:16 +0200

    created and implemented IProductAsyncRepository

---
+++ crud/crud.Core/Interfaces/IProductAsyncRepository.cs
@@ -0,0 +1,16 @@
+﻿using crud.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud.Core.Interfaces
+{
+    interface IProductAsyncRepository
+    {
+        Task<ProductEntity> GetProductAsync(int id);
+        Task<IEnumerable<ProductEntity>> GetProductsAsync();
+        void AddProduct(ProductEntity product);
+        Task<bool> SaveChangesAsync();
+    }
+}
